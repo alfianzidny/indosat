d012777ade44ee6abe16bf369a4f0be26cc4cd2b
usertersakiti <51637256+usertersakiti@users.noreply.github.com>
2019-06-10 19:46:42 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!usr/bin/bash
+#User-Tersakiti
+
+b='\033[34;1m'
+g='\033[32;1m'
+
+clear
+
+figlet -f slant "Inject Indosat" | lolcat
+echo $g"-----------------------------------"
+echo $b "Uploader By User_Tersakiti"
+echo $b "Https://github.com/usertersakiti"
+echo $g "----------------------------------"
+sleep 2
+echo "Installing.."
+sleep 4
+apt update && apt upgrade -y
+apt install curl
+apt install python2
+clear
+python2 main.py
+
+
+
+#Mau Ngapain Dek ?
